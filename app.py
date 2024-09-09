7de2ecbc2a97f6a3c8c73d3cd1a8c9a26e9da10f
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-09 14:18:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def main_dashboard():
     # Rename Cols / Clean Up Df
     data = st.session_state.full_data
     data.columns = data.columns.str.replace('__Facebook_Ads', '', regex=False)
+    data.columns = data.columns.str.replace('_', ' ', regex=False)
     st.write(data)
 
 password_protection()
