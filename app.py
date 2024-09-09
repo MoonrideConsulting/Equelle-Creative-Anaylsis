ffefefdb16faeea8336fccbc07e2353f9e5043c4
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-09 16:06:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main_dashboard():
 
     num_combos = st.number_input("Pick a number", 2, 4)
 
-    st.write(cross_section_analysis(data, num_combos))
+    st.dataframe(cross_section_analysis(data, num_combos))
 
 
 password_protection()
