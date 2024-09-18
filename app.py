86d9f6d3534db3b917e2178ba9dee348ba0a3588
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-18 17:35:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import Cross_section
 # Main function to control navigation
 def main_dashboard():
     # Set up navigation for different pages (using radio buttons for tabs)
-    page = st.radio("Select a page", ["Overview", "Cross Section Analysis", "ML Analysis"])
+    page = st.radio("Select a page", ["Cross Section Analysis", "Ranked Combinations"], default=["Cross Section Analysis"])
 
-    if page == "Overview":
+    if page == "Cross Section Analysis":
+        st.markdown("<h1 style='text-align: center;'>Cross Section Analysis</h1>", unsafe_allow_html=True)
+        Cross_section.password_protection()  # Call the main() function from cross_section_analysis.py
+    
+    elif page == "Cross Section Analysis":
         st.markdown("<h1 style='text-align: center;'>Overview</h1>", unsafe_allow_html=True)
         st.write("This is the overview page where you can provide general insights, summaries, or other key metrics.")
         st.write("Add charts or summaries here...")
-
-    elif page == "Cross Section Analysis":
-        st.markdown("<h1 style='text-align: center;'>Cross Section Analysis</h1>", unsafe_allow_html=True)
-        Cross_section.password_protection()  # Call the main() function from cross_section_analysis.py
 
 # Run the dashboard
 if __name__ == "__main__":
