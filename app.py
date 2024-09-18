c1cd2add010bfb02212bce9b69806ab62e371e82
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-18 18:40:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main_dashboard():
 
     st.markdown("<h1 style='text-align: center;'>Equelle Creative Analysis</h1>", unsafe_allow_html=True)
     # Set up navigation for different pages (using radio buttons for tabs)
-    page = st.radio("Select a page", ["Cross Section Analysis", "Ranked Combinations"], index = 0)
+    page = st.radio("Select a page", ["Cross Section Analysis", "Ranked Combinations", "Machine Learning Analyis"], index = 0)
 
     if page == "Cross Section Analysis":
         st.markdown("<h2 style='text-align: center;'>Cross Section Analysis</h2>", unsafe_allow_html=True)
@@ -18,6 +18,10 @@ def main_dashboard():
     elif page == "Ranked Combinations":
         st.markdown("<h2 style='text-align: center;'>Ranked Combinations</h2>", unsafe_allow_html=True)
         ranked_combos.main()
+
+    elif page == "Machine Learning Analyis":
+        st.markdown("<h2 style='text-align: center;'>Machine Learning Analyis</h2>", unsafe_allow_html=True)
+        machine_learning.main()
 
 # Run the dashboard
 if __name__ == "__main__":
