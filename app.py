591a1dba92ee5aae3af2f00bbe1b28353576c2c8
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-18 17:59:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ import cross_section
 def main_dashboard():
 
     st.set_page_config(page_title="Equelle Creative Analysis",page_icon="🧑‍🚀",layout="wide")
-    
+
+    st.markdown("<h1 style='text-align: center;'>Equelle Creative Analysis</h1>", unsafe_allow_html=True)
     # Set up navigation for different pages (using radio buttons for tabs)
     page = st.radio("Select a page", ["Cross Section Analysis", "Ranked Combinations"], index = 0)
 
     if page == "Cross Section Analysis":
-        st.markdown("<h1 style='text-align: right;'>Cross Section Analysis</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Cross Section Analysis</h2>", unsafe_allow_html=True)
         cross_section.password_protection()  # Call the main() function from cross_section_analysis.py
     
     elif page == "Ranked Combinations":
-        st.markdown("<h1 style='text-align: right;'>Ranked Combinations</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: right;'>Ranked Combinations</h2>", unsafe_allow_html=True)
         st.write("This is the overview page where you can provide general insights, summaries, or other key metrics.")
         st.write("Add charts or summaries here...")
 
