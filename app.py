8f43894f5a03c174d98ebe6259f578fa82851d31
Russ-Moonride <140208843+Russ-Moonride@users.noreply.github.com>
2024-09-19 11:18:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ import machine_learning
 # Main function to control navigation
 def main_dashboard():
 
-    st.set_page_config(page_title="Equelle Creative Analysis",page_icon="🧑‍🚀",layout="wide")
+    st.set_page_config(page_title="Equelle Creative Analysis", page_icon="🧑‍🚀", layout="wide")
 
-    st.markdown("<h1 style='text-align: center;'>Equelle Creative Analysis</h1>", unsafe_allow_html=True)
-    # Set up navigation for different pages (using radio buttons for tabs)
-    page = st.radio("Select a page", ["Cross Section Analysis", "Ranked Combinations", "Combo Breakdown", "Machine Learning Analysis"], index = 0)
+    # Sidebar for navigation
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Cross Section Analysis", "Ranked Combinations", "Combo Breakdown", "Machine Learning Analysis"], index=0)
 
-    if page == "Cross Section Analysis":
+    # Set page header and layout
+    st.markdown("<h1 style='text-align: center;'>Equelle Creative Analysis Dashboard</h1>", unsafe_allow_html=True)
+
+     if page == "Cross Section Analysis":
         st.markdown("<h2 style='text-align: center;'>Cross Section Analysis</h2>", unsafe_allow_html=True)
         cross_section.password_protection()  # Call the main() function from cross_section_analysis.py
     
